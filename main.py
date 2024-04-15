e1434f0f37f331b1abd03341a611ac7c42f0ebd9
BiraBalaZ <erick.m.anjos@gmail.com>
2024-04-15 15:35:55 -0300

    Commiting first chalenge

--- main.py
+++ main.py
@@ -9,19 +9,33 @@ motorcycleB = classes.MotorcycleB()
 motorcycleC = classes.MotorcycleC()
 
 cars = [carA, carB, carC, motorcycleA, motorcycleB, motorcycleC]
+cabecalho = ['Year', 'Model', 'Motor', 'Color', 'Velocity', 'Wheels', 'Step']
 
 system('cls')
-print('+--------+---------+---------+---------+----------+---------+--------+')
-print('|  Year  |  Model  |  Motor  |  Color  | Velocity |  Wheels |  Step  |')
-print('+--------+---------+---------+---------+----------+---------+--------+')
+
+def line():
+    '''Docstring'''
+    for j in range(0, len(cabecalho)):
+        tamanho = len(cabecalho[j]) + 4
+        print(f'+' + '-' * tamanho, end = '')
+    print('+\n', end = '')
+
+line()
+
+for j in range(0, len(cabecalho)):
+    tamanho = len(cabecalho[j]) + 4
+    print(f'|  {cabecalho[j]}  ', end = '')
+print('|\n', end = '')
+
+line()
 
 for i in range(0, len(cars)):
     print(f'| {cars[i].year:^7}', end='')
     print(f'| {cars[i].model:^7}', end=' ')
-    print(f'| {cars[i].mark:^7}', end=' ')
+    print(f'| {cars[i].mark:7}', end=' ')
     print(f'| {cars[i].color:^7}', end=' ')
-    print(f'|  {cars[i].velocity:^7}', end=' ')
-    print(f'| {cars[i].wheels:^7}', end=' ')
+    print(f'|  {cars[i].velocity:^9}', end=' ')
+    print(f'| {cars[i].wheels:^8}', end=' ')
     print(f'| {cars[i].step:^7}|')
 
-print('+--------+---------+---------+---------+----------+---------+--------+')
+line()
